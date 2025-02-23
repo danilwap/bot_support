c56e24c8ba5b2e1b153f4e74b8ad81d3d849c81e
Danila Beskrokov <beskrokovd@mail.ru>
2025-02-23 19:16:29 +0300

    fixed bag reason

--- db_logic/base.py
+++ db_logic/base.py
@@ -24,6 +24,9 @@ class SupportTickets(Base):
 
     @classmethod
     def create_ticket(cls, tg_id: int, message_thread_id: int) -> None:
+        if cls.get_ticket_by_tg_id(tg_id):
+            cls.delete_ticket_by_tg_id(tg_id)
+
         session = Session()
         ticket = cls(tg_id=tg_id, message_thread_id=message_thread_id)
         session.add(ticket)
@@ -52,12 +55,36 @@ class SupportTickets(Base):
         session.commit()
         session.close()
 
+    @classmethod
+    def delete_ticket_by_tg_id(cls, tg_id: int) -> None:
+        session = Session()
+        ticket = session.query(cls).filter_by(tg_id=tg_id).first()
+        session.delete(ticket)
+        session.commit()
+        session.close()
+
+# Для списка всех тикетов и установки им порядковых номеров, нужны две функции, создание нового и получание последнего
 class AllTickets(Base):
     __tablename__ = 'all_tickets'
 
     id = Column(Integer, primary_key=True)
     user_id_tg = Column(Integer)
-    link_for_ticket = Column(String)
+
+    @classmethod
+    def create_ticket(cls, user_id_tg: int) -> None:
+        session = Session()
+        ticket = cls(user_id_tg=user_id_tg)
+        session.add(ticket)
+        session.commit()
+        session.close()
+
+    @classmethod
+    def get_ticket_by_tg_id(cls, user_id_tg: int) -> Type['SupportTickets'] | None:
+        session = Session()
+        ticket = session.query(cls).filter_by(user_id_tg=user_id_tg).order_by(cls.id.desc()).first()
+        session.close()
+        return ticket
+
 
 
 # Создаем таблицу в базе данных, если она не существует
